34383baa9456e2c21132579670b8d2706aa0291e
SolehaOdilova <odilovasoleha73@gmail.com>
2025-07-06 21:27:33 +0000

    Установлен python-dotenv и обновлён requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['89.169.179.113', '127.0.0.1', 'localhost','taskihost.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
